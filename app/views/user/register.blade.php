66c90131b9414e0b37c572293236485ffe23fe82
Rodrigo Espinosa <espinosacurbelo@gmail.com>
2013-12-02 03:13:23 -0200

    User register view

---
+++ app/views/user/register.blade.php
@@ -0,0 +1,80 @@
+@extends('layouts.default')
+
+@section('main')
+<section id='register'>
+	@if(isset($messages))
+		<h3>Hay errores en el formulario</h3>
+		<ul class='register_errors'>
+			@foreach($messages->all() as $message)
+				<li>{{ $message }}</li>
+			@endforeach
+		</ul>
+	@endif
+	{{ Form::open(array('route' => 'user.store')) }}
+		<h3>Tipo de cuenta</h3>
+		<div class='row'>
+			{{ Form::radio('role', 'Persona', true, array('id' => 'role_persona')) }}
+			{{ Form::label('role_persona', 'Personal') }}
+			{{ Form::radio('role', 'Empresa', false, array('id' => 'role_empresa')) }}
+			{{ Form::label('role_empresa', 'Empresarial') }}
+		</div>
+		<h3>Persona Responsable</h3>
+		<div class='row'>
+			<div>
+				{{ Form::label('name', 'Nombre') }}
+				{{ Form::text('name') }}
+			</div>
+			<div>
+				{{ Form::label('lastname', 'Apellido') }}
+				{{ Form::text('lastname') }}
+			</div>
+			<div>
+				{{ Form::label('password', 'Contraseña') }}
+				{{ Form::password('password') }}
+			</div>
+			<div>
+				{{ Form::label('password_confirmation', 'Repetir contraseña') }}
+				{{ Form::password('password_confirmation') }}
+			</div>
+		</div>
+		<div class='row'>
+			<div>
+				{{ Form::label('email', 'Email') }}
+				{{ Form::email('email') }}
+			</div>
+			<div>
+				{{ Form::label('phone', 'Phone') }}
+				{{ Form::text('phone') }}
+			</div>
+			<div>
+				{{ Form::label('company_name', 'Razón social') }}
+				{{ Form::text('company_name') }}
+			</div>
+			<div>
+				{{ Form::label('rut', 'RUT') }}
+				{{ Form::text('rut') }}
+			</div>
+		</div>
+		<div class='row-2'>
+			<div>
+				<h3>Dirección de envio</h3>
+				{{ Form::label('shiping_address', 'Dirección') }}
+				{{ Form::text('shiping_address') }}
+				{{ Form::label('shipping_time_from', 'Horario preferencial') }}
+				{{ Form::select('shipping_time_from', $times) }}
+				<span>a</span>
+				{{ Form::select('shipping_time_to', $times) }}
+			</div>
+			<div>
+				<h3>Dirección de facturación</h3>
+				{{ Form::checkbox('same_billing_address') }}
+				{{ Form::label('same_billing_address', 'Igual que la dirección de envio') }}
+				{{ Form::text('billing_address') }}
+			</div>
+		</div>
+
+		{{ Form::button('Cancelar') }}
+		{{ Form::submit('Registrarme') }}
+	{{ Form::close() }}
+</section>
+@stop
