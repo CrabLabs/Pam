7b4dae7e6a67f17fd02391b968adad01f40003c2
Rodrigo Espinosa <espinosacurbelo@gmail.com>
2013-12-02 03:12:34 -0200

    Orders Advance View

--- app/views/products/orders.blade.php
+++ app/views/products/orders.blade.php
@@ -2,54 +2,35 @@
 
 @section('main')
 	{{ Form::open() }}
-		<select name='product_id'>
+		<section class='orders_step_1'>
 			@foreach($products as $product)
-				<option value='{{ $product->id }}'>{{ $product->name }}</option>
+				<a href='#' data-id='{{ $product->id }}' class='product_photo'>
+					{{ HTML::image($product->image) }}
+					<span>{{ $product->name }}</span>
+				</a>
 			@endforeach
-		</select>
-		
-		<select name='amount'></select>
+		</section>
+		<section class='orders_step_2'>
+			<select name='product_id'>
+				@foreach($products as $product)
+					<option value='{{ $product->id }}'>{{ $product->name }}</option>
+				@endforeach
+			</select>
+			
+			<select name='amount'></select>
 
-		<select name='size'></select>
+			<select name='size'></select>
 
-		<select name='inks'></select>
-		
-		<span id='cost'></span>
+			<select name='inks'></select>
+			
+			<span id='cost'></span>
+		</section>
+		{{ Form::submit() }}
 	{{ Form::close() }}
 @stop
 
 @section('scripts')
 	@parent
 
-	<script type='text/javascript'>
-	(function ($) {
-		'use strict';
-
-		var data, $self, html, next;
-
-		$(document).on('ready', function () {
-			$('form select:first').trigger('change');
-		});
-
-		$('form select').on('change', function () {
-			$self = $(this);
-			data = $('form').serialize();
-			data = data.split($(this).next('select').attr('name'))[0];
-			data+= '&select=' + ($(this).next('select').attr('name') || 'cost');
-			
-			$.getJSON('order/getDetail', data, function (res) {
-				html = '';
-				next = $self.next('select').attr('name');
-				if (res.cost !== undefined) {
-					$('#cost').text(res.cost);
-				} else {
-					$.each(res, function (index, value) {
-						html += '<option>' + value[next] + '</option>';
-					});
-					$self.next('select').html('').append(html).trigger('change');
-				}
-			});
-		});
-	}(jQuery));
-	</script>
+	{{ HTML::script('js/orders.js') }}
 @stop
---
+++ public/js/orders.js
@@ -0,0 +1,50 @@
+(function ($) {
+	'use strict';
+
+	var data, $self, html, next, currentStep;
+
+	$(document).on('ready', function () {
+		$('form select:first').trigger('change');
+		currentStep = 1;
+		$('.orders_step_2').hide();
+		$('.orders_step_1').show();
+	});
+
+	$('.product_photo').on('click', function (event) {
+		event.preventDefault();
+
+		$('select[name=\'product_id\']').val($(this).data('id')).trigger('change');
+		$('.orders_step_1').hide();
+		$('.orders_step_2').show();
+	});
+
+	$('form').on('submit', function (event) {
+		event.preventDefault();
+
+		switch (currentStep) {
+			case 1: $('.orders_step_1').hide(); $('.orders_step_2').show(); break;
+			case 2: $(this).submit(); break;
+		}
+		currentStep += 1;
+	});
+
+	$('form select').on('change', function () {
+		$self = $(this);
+		data = $('form').serialize();
+		data = data.split($(this).next('select').attr('name'))[0];
+		data+= '&select=' + ($(this).next('select').attr('name') || 'cost');
+		
+		$.getJSON('order/getDetail', data, function (res) {
+			html = '';
+			next = $self.next('select').attr('name');
+			if (res.cost !== undefined) {
+				$('#cost').text(res.cost);
+			} else {
+				$.each(res, function (index, value) {
+					html += '<option>' + value[next] + '</option>';
+				});
+				$self.next('select').html('').append(html).trigger('change');
+			}
+		});
+	});
+}(jQuery));
