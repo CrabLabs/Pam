825405affa4958a39e1fc01242b9201e8a39c4a9
Rodrigo Espinosa <espinosacurbelo@gmail.com>
2013-12-14 23:53:40 -0200

    User edit basic style

--- app/views/user/edit.blade.php
+++ app/views/user/edit.blade.php
@@ -1,89 +1,102 @@
 @extends('layouts.default')
 
 @section('main')
-<section id='edit' class='container whiteBackground'>
-	<h3 class='title'>Datos personales</h3>
-	@if(isset($messages))
-		<h3>Hay errores en el formulario</h3>
-		<ul class='register_errors'>
-			@foreach($messages->all() as $message)
-				<li>{{ $message }}</li>
-			@endforeach
-		</ul>
-	@endif
-	{{ Form::open(array('route' => 'user.store')) }}
-		<div class='section'>
-			<h3>Tipo de cuenta</h3>
-			<div class='row account_type'>
-				{{ Form::radio('role', 'Persona', true, array('id' => 'role_persona')) }}
-				{{ Form::label('role_persona', 'Personal') }}
-				{{ Form::radio('role', 'Empresa', false, array('id' => 'role_empresa')) }}
-				{{ Form::label('role_empresa', 'Empresarial') }}
-			</div>
-		</div>
-		<div class='section'>
-			<h3>Persona Responsable</h3>
-			<div class='row'>
-				<div>
-					{{ Form::label('name', 'Nombre') }}
-					{{ Form::text('name') }}
-				</div>
-				<div>
-					{{ Form::label('lastname', 'Apellido') }}
-					{{ Form::text('lastname') }}
-				</div>
-				<div>
-					{{ Form::label('email', 'Email') }}
-					{{ Form::email('email') }}
-				</div>
-			</div>
-			<div class='row'>
-				<div>
-					{{ Form::label('phone', 'Phone') }}
-					{{ Form::text('phone') }}
-				</div>
-				<div>
-					{{ Form::label('company_name', 'Razón social') }}
-					{{ Form::text('company_name') }}
-				</div>
-				<div>
-					{{ Form::label('rut', 'RUT') }}
-					{{ Form::text('rut') }}
-				</div>
-			</div>
-			<div class='row'>
-				<div>
-					{{ Form::label('password', 'Contraseña') }}
-					{{ Form::password('password') }}
-				</div>
-				<div>
-					{{ Form::label('password_confirmation', 'Repetir contraseña') }}
-					{{ Form::password('password_confirmation') }}
-				</div>
-			</div>
-		</div>
-		<div class='section'>
-			<div class='row-2'>
-				<div>
-					<h3>Dirección de envio</h3>
-					{{ Form::label('shiping_address', 'Dirección') }}
-					{{ Form::text('shiping_address') }}
-					{{ Form::label('shipping_time_from', 'Horario preferencial') }}
-					{{ Form::select('shipping_time_from', $times) }}
-					<span>a</span>
-					{{ Form::select('shipping_time_to', $times) }}
-				</div>
-				<div>
-					<h3>Dirección de facturación</h3>
-					{{ Form::checkbox('same_billing_address') }}
-					{{ Form::label('same_billing_address', 'Igual que la dirección de envio') }}
-					{{ Form::text('billing_address') }}
-				</div>
-			</div>
-		</div>
+<section class='container upper'>
+	<h3>Mi cuenta</h3>
+	<p>Recuerda tener tus datos actualizados para poder enviarte correctamente los trabajos.</p>
+</section>
 
-		{{ Form::button('Cancelar') }}
-		{{ Form::submit('Registrarme') }}
-	{{ Form::close() }}
+<section id='edit' class='container whiteBackground'>
+	<div class='edit-main'>
+		<h3 class='title'>Datos personales</h3>
+		@if(isset($messages))
+			<h3>Hay errores en el formulario</h3>
+			<ul class='register_errors'>
+				@foreach($messages->all() as $message)
+					<li>{{ $message }}</li>
+				@endforeach
+			</ul>
+		@endif
+		{{ Form::open(array('route' => 'user.store')) }}
+			<div class='section'>
+				<h3>Tipo de cuenta</h3>
+				<div class='row account_type'>
+					{{ Form::radio('role', 'Persona', true, array('id' => 'role_persona')) }}
+					{{ Form::label('role_persona', 'Personal') }}
+					{{ Form::radio('role', 'Empresa', false, array('id' => 'role_empresa')) }}
+					{{ Form::label('role_empresa', 'Empresarial') }}
+				</div>
+			</div>
+			<div class='section'>
+				<h3>Persona Responsable</h3>
+				<div class='row'>
+					<div>
+						{{ Form::label('name', 'Nombre') }}
+						{{ Form::text('name') }}
+					</div>
+					<div>
+						{{ Form::label('lastname', 'Apellido') }}
+						{{ Form::text('lastname') }}
+					</div>
+					<div>
+						{{ Form::label('email', 'Email') }}
+						{{ Form::email('email') }}
+					</div>
+				</div>
+				<div class='row'>
+					<div>
+						{{ Form::label('phone', 'Phone') }}
+						{{ Form::text('phone') }}
+					</div>
+					<div>
+						{{ Form::label('company_name', 'Razón social') }}
+						{{ Form::text('company_name') }}
+					</div>
+					<div>
+						{{ Form::label('rut', 'RUT') }}
+						{{ Form::text('rut') }}
+					</div>
+				</div>
+				<div class='row'>
+					<div>
+						{{ Form::label('password', 'Contraseña') }}
+						{{ Form::password('password') }}
+					</div>
+					<div>
+						{{ Form::label('password_confirmation', 'Repetir contraseña') }}
+						{{ Form::password('password_confirmation') }}
+					</div>
+				</div>
+			</div>
+			<div class='section'>
+				<div class='row-2'>
+					<div>
+						<h3>Dirección de envio</h3>
+						{{ Form::label('shiping_address', 'Dirección') }}
+						{{ Form::text('shiping_address') }}
+						{{ Form::label('shipping_time_from', 'Horario preferencial') }}
+						{{ Form::select('shipping_time_from', $times) }}
+						<span>a</span>
+						{{ Form::select('shipping_time_to', $times) }}
+					</div>
+					<div>
+						<h3>Dirección de facturación</h3>
+						{{ Form::checkbox('same_billing_address') }}
+						{{ Form::label('same_billing_address', 'Igual que la dirección de envio') }}
+						{{ Form::text('billing_address') }}
+					</div>
+				</div>
+			</div>
+
+			{{ Form::button('Cancelar') }}
+			{{ Form::submit('Registrarme') }}
+		{{ Form::close() }}
+	</div>
+	<aside>
+		<ul>
+			<li class='active'><a href='#'>Datos personales</a></li>
+			<li><a href='#'>Mis pedidos</a></li>
+		</ul>
+	</aside>
 </section>
 @stop
