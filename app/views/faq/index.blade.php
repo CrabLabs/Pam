2122b5397d969b91398635593616040b0c319a5c
Rodrigo Espinosa <espinosacurbelo@gmail.com>
2013-12-18 21:04:20 -0200

    FAQ style

--- app/views/faq/index.blade.php
+++ app/views/faq/index.blade.php
@@ -1,6 +1,10 @@
 @extends('layouts.default')
 
 @section('main')
+<section class='upper'>
+	<h3>Preguntas frecuentes</h3>
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+</section>
 <section id='faq' class='container whiteBackground'>
 	@foreach($faqs as $faq)
 		<div class='faq'>
@@ -8,6 +12,11 @@
 			<div class='answer'>{{ $faq->answer }}</div>
 		</div>
 	@endforeach
+</section>
+<section class='container greyBackground'>
+	<h3>¿No encontraste una respuesta?</h3>
+	<p>Puedes utilizar nuestra sección {{ HTML::link(URL::to('contact'), 'Contactos') }} para enviarnos tu consulta. También puede comunicarte telefónicamente con nosotros para que podamos ayudarte.<br><br>
+	Tel: 2208 50 92 o por mail a <a href='mailto:info@pam.com.uy'>info&#64;pam.com.uy</a></p>
 </section>
 @stop
 
