59639d440bcdec921abb3b63b751764894161b3c
Rodrigo Espinosa <espinosacurbelo@gmail.com>
2013-12-26 03:29:45 -0200

    Orders UX Advance

--- app/views/products/orders.blade.php
+++ app/views/products/orders.blade.php
@@ -94,11 +94,28 @@
 					{{ Form::email('email') }}
 				</div>
 			</div>
+			<div>
+				{{ Form::button('Siguiente', array('class' => 'btn green next_step')) }}
+			</div>
 		</section>
 		<section class='orders_step_3'>
-			<!--<span id='cost' class='budgetable'></span>-->
+			<div class='details_list'>
+				<h3>Producto</h3>
+				<dl></dl>
+				<a href='#' class='modify'>Modificar</a>
+			</div>
+			<div class='budget'>
+				<h3>Presupuesto</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				<span id='cost' class='budgetable'>$ 2873</span>
+			</div>
+			{{ Form::submit() }}
 		</section>
-		{{ Form::submit() }}
 	{{ Form::close() }}
 </section>
 @stop
--- public/js/orders.js
+++ public/js/orders.js
@@ -1,13 +1,12 @@
 (function ($) {
 	'use strict';
 
-	var data, $self, html, next, currentStep, budgetables, nextSelect;
+	var data, $self, html, next, budgetables, nextSelect;
 
 	$(document).on('ready', function () {
-		currentStep = 1;
 		budgetables = $('input[name=budgetables').val().split(',');
 		$('form select:first').trigger('change');
-		$('.orders_step_2').hide();
+		$('.orders_step_2, .orders_step_3').hide();
 		$('.orders_step_1').show();
 	});
 
@@ -20,27 +19,48 @@
 		$('nav.steps .active').removeClass('active').next('li').addClass('active');
 	});
 
+	$('.next_step').on('click', function () {
+		if (!($('select[name=product_id]').val() in budgetables)) {
+			alert('Mensaje');
+		} else {
+			$('.orders_step_2').hide();
+			$('.orders_step_3').show();
+			$('nav.steps .active').removeClass('active').next('li').addClass('active');	
+		}
+		$('.details_list dl').html('<dt>Producto:</dt><dd>' + $('select[name=product_id] :selected').text() + '</dd>');
+		$('.details .budgetable').each(function () {
+			var html = '<dt>' + $(this).children('label').text() + '</dt>';
+			html += '<dd>' + $(this).children('select').val() + '</dd>';
+			$('.details_list dl').append(html);
+		});
+	});
+
+	$('.modify').on('click', function (event) {
+		event.preventDefault();
+
+		$('.orders_step_3').hide();
+		$('.orders_step_2').show();
+		$('nav.steps .active').removeClass('active').prev('li').addClass('active');	
+	});
+
 	$('form').on('submit', function (event) {
 		event.preventDefault();
 
-		switch (currentStep) {
-			case 1: $('.orders_step_1').hide(); $('.orders_step_2').show(); break;
-			case 2: $(this).submit(); break;
-		}
-		currentStep += 1;
+		$(this).submit();
 	});
 
 	$('.details select').on('change', function () {
 		if (!($('select[name=product_id]').val() in budgetables)) {
+			$('.next_step').val('Presupuestar');
 			$('.budgetable').hide();
 			$('.no-budgetable').show();
 			return false;
 		} else {
+			$('.next_step').val('Siguiente');
 			$('.budgetable').show();
 			$('.no-budgetable').hide();
 		}
 		
-		// $self = $(this);
 		data = $('.details').serialize();
 		nextSelect = $(this).parent('div').next('div').children('select');
 		
